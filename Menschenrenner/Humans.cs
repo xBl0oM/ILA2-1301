eeb1e02a2948e227cbc6a3f35455f829b36c6d80
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2023-10-25 08:00:58 +0200

    figurenreset hinzugefügt

--- Menschenrenner/Humans.cs
+++ Menschenrenner/Humans.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Menschenrenner
 {
@@ -11,12 +12,14 @@ namespace Menschenrenner
         private string _name;
         private int _speed;
         private string _feature;
+        private Image _image;
 
-        public Humans(string name, int speed, string feature)
+        public Humans(string name, int speed, string feature, Image image)
         {
             _name = name;
             _speed = speed;
             _feature = feature;
+            _image = image;
         }
     }
 }
